ccc59861ea5f660d4d6a8dc9ce20bd6d57b168b6
Whyyyyyme <wahyunanda503@gmail.com>
2025-11-22 23:51:38 +0700

    test

---
+++ lib/pages/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class cobatest extends StatelessWidget {
+  const cobatest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
